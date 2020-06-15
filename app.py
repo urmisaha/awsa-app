c582e3d805bb60dab29bec5d252b77fd66e485ac
urmisaha <urmisaha.88@gmail.com>
2020-06-16 01:59:21 +0530

    front-end ready

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
-from flask import Flask, render_template
-from predict import *
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
+# from predict import *
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=('GET', 'POST'))
 def index():
+    if request.method == 'POST':
+        domain = request.form['domain']
+        text = request.form['review']
+        print(domain)
+        print(text)
+        # result = calculate_sentiment(text)
+        # if result is None:
+            # abort(404)
     return render_template('index.html')
 
 @app.route('/desc')
 def desc():
     return render_template('desc.html')
-
-@app.route('/predict')
-def predict(text):
-    result = calculate_sentiment(text)
-    if result is None:
-        abort(404)
-    return result
 
 if __name__ == "__main__":
     app.run("10.129.2.170", "5001")
